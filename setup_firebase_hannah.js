1de0f1a0ed572f5b49a83480bea0b2f6b2f27411
HannahKhuc <hannahkhuc@gmail.com>
2019-04-05 11:35:42 -0700

    added index page and JS scripts

---
+++ setup_firebase_hannah.js
@@ -0,0 +1,16 @@
+(function(){  
+
+  // Initialize Firebase
+  var config = {
+
+		apiKey: "AIzaSyDbL92JRSdB3V1OT7-iMFYytHbxq-7cs3s",
+		authDomain: "hannahproject-9a1b1.firebaseapp.com",
+		databaseURL: "https://hannahproject-9a1b1.firebaseio.com",
+		projectId: "hannahproject-9a1b1",
+		storageBucket: "hannahproject-9a1b1.appspot.com",
+		messagingSenderId: "973464007710"
+	
+  };
+  firebase.initializeApp(config);
+  app_firebase = firebase; 
+})();
