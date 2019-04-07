bb39777993f732d030a1b8fc4671caa1468e265a
Wilson Zhu <wilson_zhu@outlook.com>
2019-04-06 17:58:09 -0700

    firebase part1 for product page

---
+++ javascripts/setup_firebase_wilson.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+(function () {
+  var config = {
+    apiKey: "AIzaSyCVk8U9e_8C_TIB15RA15A29u-l9G_lrW4",
+    authDomain: "project-1320319030999349486.firebaseapp.com",
+    databaseURL: "https://project-1320319030999349486.firebaseio.com",
+    projectId: "project-1320319030999349486",
+    storageBucket: "project-1320319030999349486.appspot.com",
+    messagingSenderId: "105113284347"
+  };
+  firebase.initializeApp(config);
+})();
